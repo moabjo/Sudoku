c94693018e1b21ad020741006dee7be36b097e4c
Moa Björkman <moaviola1@hotmail.com>
2020-12-01 21:50:35 +0100

    made test cases

--- Sudoku_Solver/src/sudoku/SudokuApplication.java
+++ Sudoku_Solver/src/sudoku/SudokuApplication.java
@@ -6,27 +6,29 @@ public class SudokuApplication {
 
 	public static void main(String[] args) {
 		SudokuSolver sudokuSolver = new SudokuMain();
+		SudokuView window = new SudokuView("Sudoku", 250, 250);
+		
 		
 		//Lägg in lite siffror
 		// det här är ett lösbart sudoku
-		sudokuSolver.setNumber(0, 3, 8);
-		sudokuSolver.setNumber(0, 5, 1);
-		
-		sudokuSolver.setNumber(1, 6, 4);
-		sudokuSolver.setNumber(1, 7, 3);
-		sudokuSolver.setNumber(2, 0, 5);
-		sudokuSolver.setNumber(3, 4, 7);
-		sudokuSolver.setNumber(3, 6, 8);
-		sudokuSolver.setNumber(4, 6, 1);
-		sudokuSolver.setNumber(5, 1, 2);
-		sudokuSolver.setNumber(5, 4, 3);
-		sudokuSolver.setNumber(6, 0, 6);
-		sudokuSolver.setNumber(6, 7, 7);
-		sudokuSolver.setNumber(6, 8, 5);
-		sudokuSolver.setNumber(7, 2, 3);
-		sudokuSolver.setNumber(7, 3, 4);
-		sudokuSolver.setNumber(8, 3, 2);
-		sudokuSolver.setNumber(8, 6, 6);
+//		sudokuSolver.setNumber(0, 3, 8);
+//		sudokuSolver.setNumber(0, 5, 1);
+//		
+//		sudokuSolver.setNumber(1, 6, 4);
+//		sudokuSolver.setNumber(1, 7, 3);
+//		sudokuSolver.setNumber(2, 0, 5);
+//		sudokuSolver.setNumber(3, 4, 7);
+//		sudokuSolver.setNumber(3, 6, 8);
+//		sudokuSolver.setNumber(4, 6, 1);
+//		sudokuSolver.setNumber(5, 1, 2);
+//		sudokuSolver.setNumber(5, 4, 3);
+//		sudokuSolver.setNumber(6, 0, 6);
+//		sudokuSolver.setNumber(6, 7, 7);
+//		sudokuSolver.setNumber(6, 8, 5);
+//		sudokuSolver.setNumber(7, 2, 3);
+//		sudokuSolver.setNumber(7, 3, 4);
+//		sudokuSolver.setNumber(8, 3, 2);
+//		sudokuSolver.setNumber(8, 6, 6);
 
 
 		System.out.print(sudokuSolver.solve());
--- Sudoku_Solver/src/sudoku/Sudoku_Test.java
+++ Sudoku_Solver/src/sudoku/Sudoku_Test.java
@@ -2,13 +2,52 @@ package sudoku;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class Sudoku_Test {
+	private int[][] board;
+	private SudokuSolver sudoku;
+
+	@Before
+	public void setUp() {
+		board = new int[9][9];
+		sudoku = new SudokuMain(board);
+
+	}
+
+	@After
+	public void tearDown() {
+		sudoku.clear();
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testEmptySudoku() {
+		board = new int [][]{ { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+		
+		sudoku = new SudokuMain(board);
+		assertTrue(sudoku.solve()); // ett tomt bräde fylls automatiskt med nollor
+	}
+
+	@Test
+	public void testSolvableSudoku() {
+		board = new int[][] { { 0, 0, 8, 0, 0, 9, 0, 6, 2 }, { 0, 0, 0, 0, 0, 0, 0, 0, 5 },
+				{ 1, 0, 2, 5, 0, 0, 0, 0, 0 }, { 0, 0, 0, 2, 1, 0, 0, 9, 0 }, { 0, 5, 0, 0, 0, 0, 6, 0, 0 },
+				{ 6, 0, 0, 0, 0, 0, 0, 2, 8 }, { 4, 1, 0, 6, 0, 8, 0, 0, 0 }, { 8, 6, 0, 0, 3, 0, 1, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 4, 0, 0 } };
+		sudoku = new SudokuMain(board);
+		assertTrue(sudoku.solve());
+
+	}
+	@Test
+	public void testUnsolvableSudoku() {
+		board = new int [][] {{ 1,2,1}};
+		sudoku = new SudokuMain(board);
+		assertFalse(sudoku.solve());
 	}
 
 }
